4bf567763c0061962a6be893586a4d6e64c734dd
cq <qchen@haiyi-info.com>
2019-08-30 10:42:43 +0800

    调整jpa

--- src/main/java/com/example/service/impl/UserServiceImpl.java
+++ src/main/java/com/example/service/impl/UserServiceImpl.java
@@ -20,12 +20,12 @@ public class UserServiceImpl implements IUserService
     private UserJpaRepository userJpaRepository;
     @Autowired
     private UserRepository userRepository;
-
+    @Override
     public List<User> findAll()
     {
         return userJpaRepository.findAll();
     }
-
+    @Override
     public List<User> findByName(String name)
     {
         List<User> userList1 = userRepository.findByName1(name);
@@ -36,19 +36,19 @@ public class UserServiceImpl implements IUserService
         System.out.println("userList3:" + userList3);
         return userRepository.findByName(name);
     }
-
+    @Override
     public void saveUser(User book)
     {
         userJpaRepository.save(book);
     }
-
+    @Override
     @Cacheable("users")
     public User findOne(long id)
     {
         System.out.println("Cached Pages");
         return userJpaRepository.findOne(id);
     }
-
+    @Override
     public void delete(long id)
     {
         userJpaRepository.delete(id);
