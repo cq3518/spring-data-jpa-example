cfe45315788622e055e645e7399b3a47594410dd
cq <qchen@haiyi-info.com>
2019-09-05 11:53:28 +0800

    get请求

--- src/main/java/com/example/web/UserController.java
+++ src/main/java/com/example/web/UserController.java
@@ -3,6 +3,7 @@ package com.example.web;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -17,7 +18,7 @@ public class UserController
     @Autowired
     private IUserService userService;
 
-    @RequestMapping(value = "/add/{id}/{name}/{address}")
+    @GetMapping(value = "/add/{id}/{name}/{address}")
     public User addUser(@PathVariable int id, @PathVariable String name,
         @PathVariable String address)
     {
@@ -29,26 +30,26 @@ public class UserController
         return user;
     }
 
-    @RequestMapping(value = "/delete/{id}")
+    @GetMapping(value = "/delete/{id}")
     public void deleteBook(@PathVariable int id)
     {
         userService.delete(id);
     }
 
-    @RequestMapping(value = "/")
+    @GetMapping(value = "/list")
     public List<User> getBooks()
     {
         return userService.findAll();
     }
 
-    @RequestMapping(value = "/{id}")
+    @GetMapping(value = "/{id}")
     public User getUser(@PathVariable int id)
     {
         User user = userService.findOne(id);
         return user;
     }
 
-    @RequestMapping(value = "/search/name/{name}")
+    @GetMapping(value = "/search/name/{name}")
     public List<User> getBookByName(@PathVariable String name)
     {
         List<User> users = userService.findByName(name);
